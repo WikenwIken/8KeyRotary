da391b7910ad830e2ed412a303eca711e0d43373
WikenwIken <78408732+WikenwIken@users.noreply.github.com>
2021-06-12 03:25:04 -0500

    Add files via upload

--- 8KeyRotary/keymaps/default/keymap.c
+++ 8KeyRotary/keymaps/default/keymap.c
@@ -1,18 +1,3 @@
-/* Copyright 2020 Wikkles
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 #define _BASE 0
 #define _FN 1
@@ -31,68 +16,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-/*bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        
-    }
-    return true;
-}*/
+
 	void encoder_update_user(uint16_t index, bool clockwise) {
-		switch(get_highest_layer(layer_state)) {
+		
+		switch(layer_state) {
 			case _BASE:
+				
 				if (clockwise) {
+					
 					tap_code(KC_LCTL);
 					tap_code(KC_WH_U);
 				} else {
+					
 					tap_code(KC_LCTL);
 					tap_code(KC_WH_D);
 				}
 			case _FN:
 				if (clockwise) {
+					
 					tap_code(KC_LALT);
 					tap_code(KC_WH_U);
 				} else {
+					
 					tap_code(KC_LALT);
 					tap_code(KC_WH_D);
 				}
 		}
 	}
 	
-			
+		
 	
-	/*		if(IS_LAYER_ON(_BASE)) {
-			if (clockwise) {
-				tap_code(KC_LCTL);
-				tap_code(KC_WH_U);
-			} else {
-				tap_code(KC_LCTL);
-				tap_code(KC_WH_D);
-			}
-		} else if (IS_LAYER_ON(_FN)) {
-			if (clockwise) {
-				tap_code(KC_LALT);
-				tap_code(KC_WH_U);
-			} else {
-				tap_code(KC_LALT);
-				tap_code(KC_WH_D);
-			}
-		}
-	}*/
-
-/*
-void encoder_update_user(uint16_t index, bool clockwise) {
-	if(IS_LAYER_ON(_BASE)) {
-		if (clockwise) {
-			tap_code16(LCTL(KC_WH_U));
-		} else {
-			tap_code16(LCTL(KC_WH_D));
-		}
-	} else if (IS_LAYER_ON(_FN)) {
-		if (clockwise) {
-			tap_code16(LALT(KC_WH_U));
-		} else {
-			tap_code16(LALT(KC_WH_D));
-		}
-	}			
-}
-*/
